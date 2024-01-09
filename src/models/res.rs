9eb25ca44d95a1d1671e24800ee7679f9663b24c
Antonio Morales <antoniomoralesrocha@gmail.com>
2024-01-09 10:35:41 -0300

    feat: 🎸 changes in how serialize data

--- src/models/res.rs
+++ src/models/res.rs
@@ -5,11 +5,8 @@ use rocket::serde::{Deserialize, Serialize};
 #[serde(crate = "rocket::serde")]
 pub struct ServicesConnectionsRes {
     pub id: i64,
-    #[serde(skip_deserializing, skip_serializing_if = "Option::is_none")]
-    pub source: Option<String>,
-    #[serde(skip_deserializing, skip_serializing_if = "Option::is_none")]
-    pub target: Option<String>,
+    pub source: String,
+    pub target: String,
     pub type_name: String,
-    #[serde(skip_deserializing, skip_serializing_if = "Option::is_none")]
-    pub created_at: Option<DateTime<Utc>>,
+    pub created_at: DateTime<Utc>,
 }
--- src/repositories/catalog_teo.rs
+++ src/repositories/catalog_teo.rs
@@ -4,24 +4,15 @@ use crate::models::db::{Db, Result};
 use crate::models::res::ServicesConnectionsRes;
 
 pub async fn get_services_db(mut db: Connection<Db>) -> Result<Vec<ServicesConnectionsRes>> {
-    let res = sqlx::query!(
-        "select sc.id, so.name as source, st.name as target, tc.name as type_name, sc.created_at
+    let res: Vec<ServicesConnectionsRes> = sqlx::query_as!(ServicesConnectionsRes,
+        r"select sc.id, so.name as source, st.name as target, tc.name as type_name, sc.created_at
         from catalog_teo.services_connections sc
             inner join catalog_teo.services so on so.id = sc.source_service_id
             inner join catalog_teo.services st on st.id = sc.target_service_id
             inner join catalog_teo.type_connection tc on tc.id = sc.type_connection_id;"
     )
     .fetch_all(&mut **db)
-    .await?
-    .iter()
-    .map(|r| ServicesConnectionsRes {
-        id: r.id,
-        source: r.source.clone(),
-        target: r.target.clone(),
-        type_name: r.type_name.clone(),
-        created_at: r.created_at,
-    })
-    .collect();
+    .await?;
 
     Ok(res)
 }
